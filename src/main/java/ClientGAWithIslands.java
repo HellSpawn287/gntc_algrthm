0e3fbbb0a3f9c4a143c92af64a41584d5c642eac
HellSpawn287 <robert.prus87@gmail.com>
2020-07-05 17:47:14 +0200

    Added ClientGAWithIslands class

---
+++ src/main/java/ClientGAWithIslands.java
@@ -0,0 +1,59 @@
+import gntic_algrtm.AlgorithmEngine;
+import gntic_algrtm.Island;
+import gntic_algrtm.IslandManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientGAWithIslands {
+    public static void main(String[] args) {
+
+        AlgorithmEngine.setTOURNAMENT_Size(62);
+
+        List<Island> circleOfEvolution2_20 = createWorldOfIslands(2, 20, 8);
+        List<Island> circleOfEvolution2_40 = createWorldOfIslands(2, 40, 8);
+        List<Island> circleOfEvolution2_60 = createWorldOfIslands(2, 60, 8);
+        List<Island> circleOfEvolution5_20 = createWorldOfIslands(5, 20, 8);
+        List<Island> circleOfEvolution5_40 = createWorldOfIslands(5, 40, 8);
+        List<Island> circleOfEvolution5_60 = createWorldOfIslands(5, 60, 8);
+        List<Island> circleOfEvolution10_20 = createWorldOfIslands(10, 20, 8);
+        List<Island> circleOfEvolution10_40 = createWorldOfIslands(10, 40, 8);
+        List<Island> circleOfEvolution10_60 = createWorldOfIslands(10, 60, 8);
+        List<Island> circleOfEvolution20_20 = createWorldOfIslands(20, 20, 8);
+        List<Island> circleOfEvolution20_40 = createWorldOfIslands(20, 40, 8);
+        List<Island> circleOfEvolution20_60 = createWorldOfIslands(20, 60, 8);
+
+
+//        IslandManager.runMigration(circleOfEvolution);
+
+        IslandManager.generations(20,3, circleOfEvolution2_20);
+
+//        IslandManager.generations(20,3, circleOfEvolution2_40);
+//        IslandManager.generations(20,3, circleOfEvolution2_60);
+//        IslandManager.generations(20,3, circleOfEvolution5_20);
+//        IslandManager.generations(20,3, circleOfEvolution5_40);
+//        IslandManager.generations(20,3, circleOfEvolution5_60);
+//        IslandManager.generations(20,3, circleOfEvolution10_20);
+//        IslandManager.generations(20,3, circleOfEvolution10_40);
+//        IslandManager.generations(20,3, circleOfEvolution10_60);
+//        IslandManager.generations(20,3, circleOfEvolution20_20);
+//        IslandManager.generations(20,3, circleOfEvolution20_40);
+//        IslandManager.generations(20,3, circleOfEvolution20_60);
+
+    }
+
+    public static List<Island> createWorldOfIslands(int numberOfIslands, int populationSize, int geneNumber) {
+        if (!(AlgorithmEngine.getTOURNAMENT_Size() > populationSize)) {
+            throw new RuntimeException("Tournament size = " + AlgorithmEngine.getTOURNAMENT_Size() + " is to small for population size = " + populationSize);
+        }
+        List<Island> circleOfEvolution = new ArrayList<>();
+
+        System.out.println("Adding to the Cirle!");
+        for (int i = 0; i < numberOfIslands; i++) {
+            Island island = IslandManager.createAndPopulateIsland(populationSize, geneNumber);
+            circleOfEvolution.add(island);
+        }
+
+        return circleOfEvolution;
+    }
+}
