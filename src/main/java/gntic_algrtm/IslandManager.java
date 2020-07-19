6ced7556169a0ec797341c0aa3882b1bce4f9486
HellSpawn287 <robert.prus87@gmail.com>
2020-07-19 13:25:00 +0200

    Added method printPopulationInFile to IslandManager and NoMigrationManager classes Added file, writer, count, generationGlobal, migrationCounter variables to IslandManager and NoMigrationManager classes,

--- src/main/java/gntic_algrtm/IslandManager.java
+++ src/main/java/gntic_algrtm/IslandManager.java
@@ -1,12 +1,20 @@
 package gntic_algrtm;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static gntic_algrtm.AlgorithmEngine.evolve;
 
 public class IslandManager {
+    public static int generationGlobal = 0;
+    public static int migrationCounter;
     static List<Island> generation;
+    static PrintWriter writer = null;
+    static File file;
+    private static int count;
 
     public static Island createAndPopulateIsland(int populationSize, int geneNumber) {
         Population momsPopulation = buildPopulation(populationSize, geneNumber);
@@ -21,25 +29,29 @@ public class IslandManager {
     }
 
     public static void migration(Island from, Island destination, int counter) {
+        migrationCounter++;
         destination.receiveMigratingChromosome(true, from.getMigrating_chromosome());
         destination.setMigrating_chromosome();
-        System.out.println("\t\t\t >>> MIGRATION " + counter);
-        destination.toString();
+        System.out.println("\t\t\t >>> MIGRATION " + counter +"\n" +destination.toString());
+        printPopulationInFile(destination);
     }
 
     public static void generations(int generationNumber, int generationDistanceForMigration, List<Island> circleOfEvolution) {
-        int generationCounter = 1;
+        int generationCounter = 0;
         generation = List.copyOf(circleOfEvolution);
-        if (generationCounter <= generationNumber) {
-            if (generationCounter % generationDistanceForMigration == 0) {
+        while (generationCounter < generationNumber) {
+            if (generationCounter % generationDistanceForMigration <= 2) {
                 generation = runMigrationAndReturnGeneration(generation);
                 generationCounter++;
+                generationGlobal = generationCounter;
+
             } else {
                 System.out.println("----> NO MIGRATION");
                 generation = islandsEvolveIndependently(generation);
+                generationCounter++;
+                generationGlobal = generationCounter;
             }
         }
-
     }
 
     private static List<Island> islandsEvolveIndependently(List<Island> generation) {
@@ -47,6 +59,7 @@ public class IslandManager {
             island.setMoms(evolve(island.getMoms(), island.getDads()));
             island.setDads(evolve(island.getMoms(), island.getDads()));
             island.setMigrating_chromosome();
+            printPopulationInFile(island);
             return island;
         }).collect(Collectors.toList());
         return islandsEvolved;
@@ -78,5 +91,42 @@ public class IslandManager {
             IslandManager.migration(circleOfEvolution.get(circleOfEvolution.size() - 1), circleOfEvolution.get(0), counter);
             counter++;
         }
+    }
+
+    private static void printPopulationInFile(Island island) {
+        Population mom = island.getMoms();
+        Population dad = island.getDads();
+        file = new File("./algorithm_data/islandPopulation_" + count + ".txt");
+        file.getParentFile().mkdirs();
+
+        try {
+            System.out.println(file.exists() ? "File created: " + file.getName() : "File already exist.");
+
+            writer = new PrintWriter(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (writer == null) throw new AssertionError();
+
+        writer.println("Island ID:" + island.getId() + " \tGeneration: " + generationGlobal + "\nThe biggest phenotypes");
+        writer.print("Island " + count + ": \t mothers \t fathers \n" + "\t" + Function.findBiggestPopulationPhenotype(mom) + ",\t" + Function.findBiggestPopulationPhenotype(dad) + ",\t");
+        writer.println("\n\nThe population sum of adaptation function results");
+        writer.print("Island " + count + ": \t mothers \t fathers \n" + "\t" + Function.calculatePopulationSumOfAdaptationFunction(mom) + ",\t" + Function.calculatePopulationSumOfAdaptationFunction(dad) + ",\t");
+
+        writer.println("\n\nThe adaptation function results for each chromosomes. \nIsland " + count + ": \t mothers \t fathers \n");
+        for (int populationCursor = 0; populationCursor < AlgorithmEngine.getTOURNAMENT_Size() - 1; populationCursor++) {
+            if (writer != null) {
+                writer.print("\t" + Function.calculateAdaptationFunction(mom.getSingleChromosome(populationCursor)) + "\t" + Function.calculateAdaptationFunction(dad.getSingleChromosome(populationCursor)) + ",\t");
+            }
+        }
+
+        writer.println("\n\nThe phenotypes for each chromosomes. \nIsland " + count + ": \t mothers \t fathers \n");
+        for (int populationCursor = 0; populationCursor < AlgorithmEngine.getTOURNAMENT_Size() - 1; populationCursor++) {
+            if (writer != null) {
+                writer.print("\t" + mom.getSingleChromosome(populationCursor).getPhenotype() + ",\t" + dad.getSingleChromosome(populationCursor).getPhenotype() + ",\t");
+            }
+        }
+        writer.close();
+        count++;
     }
 }
--- src/main/java/gntic_algrtm/NoMigrationManager.java
+++ src/main/java/gntic_algrtm/NoMigrationManager.java
@@ -1,9 +1,17 @@
 package gntic_algrtm;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import static gntic_algrtm.AlgorithmEngine.evolve;
 import static gntic_algrtm.Function.findChromosomeWithBiggestPhenotype;
 
 public class NoMigrationManager {
+    public static int generationGlobal = 0;
+    static PrintWriter writer = null;
+    static File file;
+    private static int count;
 
     public static Population buildPopulation(int populationSize, int geneNumber) {
         return new Population.Builder(populationSize, geneNumber)
@@ -21,9 +29,10 @@ public class NoMigrationManager {
         if (generationCounter <= generationNumber) {
             do {
                 System.out.println("GENERATION : " + generationCounter);
-                Population temp = generations(offspring, dads);
-                offspring = temp;
-                generationCounter ++;
+                offspring = generations(offspring, dads);
+                printPopulationInFile(moms, dads);
+                generationCounter++;
+                generationGlobal = generationCounter;
             } while (generationCounter <= generationNumber);
         }
 
@@ -35,5 +44,41 @@ public class NoMigrationManager {
         Chromosome best = findChromosomeWithBiggestPhenotype(nextGeneration);
 
         return nextGeneration;
+    }
+
+    private static void printPopulationInFile(Population mom, Population dad) {
+        file = new File("./algorithm_data/islandPopulation_" + count + ".txt");
+        file.getParentFile().mkdirs();
+
+        try {
+            System.out.println(file.exists() ? "File created: " + file.getName() : "File already exist.");
+
+            writer = new PrintWriter(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (writer == null) throw new AssertionError();
+
+        writer.println("Population ID:" + mom.getId() + " \tGeneration: " + generationGlobal + "\nThe biggest phenotypes");
+        writer.print("Population " + count + ": \t mothers \t fathers \n" + "\t" + Function.findBiggestPopulationPhenotype(mom) + ",\t" + Function.findBiggestPopulationPhenotype(dad) + "\t");
+        writer.println("\n\nThe population sums of adaptation function results");
+        writer.print("Population " + count + ": \t mothers \t fathers \n" + "\t" + Function.calculatePopulationSumOfAdaptationFunction(mom) + ",\t" + Function.calculatePopulationSumOfAdaptationFunction(dad) + "\t");
+
+        writer.println("\n\nThe adaptation function results for each chromosomes. \nPopulation " + count + ": \t mothers \t fathers \n");
+        for (int populationCursor = 0; populationCursor < AlgorithmEngine.getTOURNAMENT_Size() - 1; populationCursor++) {
+            if (writer != null) {
+                writer.print("\t" + Function.calculateAdaptationFunction(mom.getSingleChromosome(populationCursor)) + "\t" + Function.calculateAdaptationFunction(dad.getSingleChromosome(populationCursor)) + ",\t");
+            }
+        }
+
+        writer.println("\n\nThe phenotypes for each chromosomes. \nPopulation " + count + ": \t mothers \t fathers \n");
+        for (int populationCursor = 0; populationCursor < AlgorithmEngine.getTOURNAMENT_Size() - 1; populationCursor++) {
+            if (writer != null) {
+                writer.print("\t" + mom.getSingleChromosome(populationCursor).getPhenotype() + ",\t" + dad.getSingleChromosome(populationCursor).getPhenotype() + ",\t");
+            }
+        }
+        writer.close();
+        count++;
     }
 }
